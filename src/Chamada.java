12afd2f605f9fece2db1ce1a8b17856136dec827
ana <anacarolineofc130@gmail.com>
2023-11-25 20:58:18 -0300

    declarando variáveis da classe chamada.

---
+++ src/Chamada.java
@@ -0,0 +1,9 @@
+import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
+
+//Declaramos as variáveis 
+public class Chamada {
+    private GregorianCalendar data;
+    private int duracao;
+}
+    
