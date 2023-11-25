3091af6848e14c45eb753438d2bd6c8edea546b2
ana <anacarolineofc130@gmail.com>
2023-11-25 20:55:42 -0300

    add construtor e retornando o cpf.

--- src/Assinantes.java
+++ src/Assinantes.java
@@ -5,5 +5,18 @@ public class Assinantes {
     private int numero;
     protected int numChamadas;
     protected Chamada[] chamadas;
-}
-   
+
+    // Construtor
+    public Assinantes(long cpf, String nome, int numero) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.numero = numero;
+        this.chamadas = new Chamada[10];
+        this.numChamadas = 0;
+    }
+
+    // Pegando o CPF
+        public Long getCpf() {
+        return cpf;
+    }
+ 
