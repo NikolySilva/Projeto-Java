2a199a191e7e3be9001ae03a5e9ccee59bddee4c
ana <anacarolineofc130@gmail.com>
2023-11-25 21:01:10 -0300

    classe Chamada finalizada.

--- src/Chamada.java
+++ src/Chamada.java
@@ -23,4 +23,13 @@ public class Chamada {
         return duracao;
     }
 
+    // Método para obter uma representação textual da chamada
     
+@Override
+public String toString() {
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    return "Data: " + sdf.format(data.getTime()) + "\nDuracao: " + duracao + " minutos"; 
+    //O código retorna uma string que combina a data formatada usando um objeto SimpleDateFormat chamado sdf e a duração (em minutos). 
+    // A string resultante parece representar informações sobre uma data e sua duração, formatadas de maneira legível.
+}
+}
