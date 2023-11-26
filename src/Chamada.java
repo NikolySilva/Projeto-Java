288ab4d6caa1ccaa8a087f8f99db7373517852f8
ana <anacarolineofc130@gmail.com>
2023-11-25 21:00:21 -0300

    add o método construtor, retornar data e duração.

--- src/Chamada.java
+++ src/Chamada.java
@@ -5,5 +5,22 @@ import java.text.SimpleDateFormat;
 public class Chamada {
     private GregorianCalendar data;
     private int duracao;
-}
+
+    // Construtor
+    public Chamada(GregorianCalendar data, int duracao) {
+        this.data = data;
+        this.duracao = duracao;
+    }
+
+    // Método para obter a data (função que vai retornar a data)
+
+    public GregorianCalendar getData() {
+        return data;
+    }
+
+    // Método para obter a duração (função que vai retornar a duração)
+    public int getDuracao() {
+        return duracao;
+    }
+
     
